3d657635e38ae5f53d6432a26145fa620ed6e9e7
Jeanne <jeannefernanda.m@gmail.com>
2024-06-04 16:56:36 -0400

    Operacoes Basicas Map: Testando os Métodos

--- src/map/AgendaContatos.java
+++ src/map/AgendaContatos.java
@@ -32,4 +32,28 @@ public class AgendaContatos {
         }
         return numeroPorNome;
     }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("Camila", 5665);
+        agendaContatos.adicionarContato("Camila Cavalcante", 1111111);
+        agendaContatos.adicionarContato("Camila DIO", 654987);
+        agendaContatos.adicionarContato("Maria Silva", 1111111);
+        agendaContatos.adicionarContato("Camila", 44444);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Maria Silva");
+
+        agendaContatos.exibirContatos();
+
+        System.out.println("O numero é " + agendaContatos.pesquisarPorNome("Camila DIO"));
+
+        agendaContatos.removerContato("Camila");
+
+        agendaContatos.exibirContatos();
+
+    }
 }
