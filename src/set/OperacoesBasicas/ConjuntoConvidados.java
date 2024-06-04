7f1c10d1771c0d749212d0f12895396f4965e824
Jeanne <jeannefernanda.m@gmail.com>
2024-06-04 15:46:03 -0400

    Operações Básicas Set: Testando os Métodos

--- src/set/OperacoesBasicas/ConjuntoConvidados.java
+++ src/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -34,5 +34,21 @@ public class ConjuntoConvidados {
         System.out.println(convidadoSet);
     }
 
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
 
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+
+        conjuntoConvidados.exibirCOnvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de Convidados");
+        conjuntoConvidados.exibirCOnvidados();
+    }
 }
