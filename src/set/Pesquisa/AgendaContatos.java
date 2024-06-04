de82a2b93557397d63eec64a74cba8589d21de5e
Jeanne <jeannefernanda.m@gmail.com>
2024-06-04 16:00:09 -0400

    Pesquisa Set: Testando os Métodos

--- src/set/Pesquisa/AgendaContatos.java
+++ src/set/Pesquisa/AgendaContatos.java
@@ -40,5 +40,25 @@ public class AgendaContatos {
         return contatoAtualizado;
     }
 
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("Camila", 0);
+        agendaContatos.adicionarContato("Camila Cavalcante", 1111111);
+        agendaContatos.adicionarContato("Camila DIO", 654987);
+        agendaContatos.adicionarContato("Maria Silva", 1111111);
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Camila"));
+
+        System.out.println("Contato Atualizado" + agendaContatos.atualizarCOntato("Maria Silva", 5555555));
+
+        agendaContatos.exibirContatos();
+    }
+
 
 }
