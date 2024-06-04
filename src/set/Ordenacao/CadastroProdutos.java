8305ff7697cde38caa45521f684d93a2aa66ca16
Jeanne <jeannefernanda.m@gmail.com>
2024-06-04 16:32:37 -0400

    Ordenacao Set: Testando os Métodos

--- src/set/Ordenacao/CadastroProdutos.java
+++ src/set/Ordenacao/CadastroProdutos.java
@@ -24,8 +24,24 @@ public class CadastroProdutos {
 
     public Set<Produto>  exibirProdutosPorPreco(){
         Set<Produto> produtoPorPreco = new TreeSet<>(new ComparatorPorPreco());
-        produtoSet.addAll(produtoSet);
+        produtoPorPreco.addAll(produtoSet);
         return produtoPorPreco;
+    }
+
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        cadastroProdutos.adicionarProduto(1, "Produto 5", 15, 5);
+        cadastroProdutos.adicionarProduto(2, "Produto 0", 20, 10);
+        cadastroProdutos.adicionarProduto(1, "Produto 3", 10, 2);
+        cadastroProdutos.adicionarProduto(9, "Produto 9", 2, 2);
+
+        System.out.println(cadastroProdutos.produtoSet);
+
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
     }
 
 
